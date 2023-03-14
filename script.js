10c41caf088b7de01433ee54b0b462c556a5c586
Sina Tabakhi <stabakhi1@shefffield.ac.uk>
2023-03-14 22:41:42 +0000

    Create a redirection page

---
+++ script.js
@@ -0,0 +1,9 @@
+var secondsLeft = 6;
+function updateTimer() {
+    secondsLeft--;
+    document.querySelector('.timer').textContent = secondsLeft;
+    if (secondsLeft <= 0) {
+        window.location.href = 'https://shef-ai.github.io/';
+    }
+}
+setInterval(updateTimer, 1000);
